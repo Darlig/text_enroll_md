ffba75de64d93e701ca96d5f14cccdc328c3de62
Maplewood <qlwy1991@gmail.com>
2024-11-18 14:54:19 +0800

    add no align loss version of TransformerKWS model

--- model/__init__.py
+++ model/__init__.py
@@ -5,11 +5,15 @@ from model import AEDKWSASRPhone
 from model import AEDASR
 from model import AEDKWSASRPhoneUnet
 from model import TransformerKWSPhone
+from model import TransformerKWSPhone_noaliloss
+from model import TransformerKWSPhone_eval_steps
 
 m_dict = {
     'AEDKWSASR': AEDKWSASR.AEDKWSASR,
     'AEDKWSASRPhone': AEDKWSASRPhone.AEDKWSASRPhone,
     'AEDASR': AEDASR.AEDASR,
     'AEDKWSASRPhoneUnet': AEDKWSASRPhoneUnet.AEDKWSASRPhoneUnet,
-    'TransformerKWSPhone': TransformerKWSPhone.TransformerKWSPhone
+    'TransformerKWSPhone': TransformerKWSPhone.TransformerKWSPhone,
+    'TransformerKWSPhone_noaliloss': TransformerKWSPhone_noaliloss.TransformerKWSPhone_noaliloss,
+    'TransformerKWSPhone_eval_steps': TransformerKWSPhone_eval_steps.TransformerKWSPhone_eval_steps
 }

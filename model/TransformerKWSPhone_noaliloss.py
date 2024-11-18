c862a8fc38ab1036be7f443c9d095eeb44365e9d
Maplewood <qlwy1991@gmail.com>
2024-11-18 14:58:57 +0800

    update transformer kws models to individual block number config

--- model/TransformerKWSPhone_noaliloss.py
+++ model/TransformerKWSPhone_noaliloss.py
@@ -66,8 +66,9 @@ class TransformerKWSPhone_noaliloss(nn.Module):
         self,
         audio_net_config,
         kw_net_config,
-        num_audio_block=8,
+        num_audio_block=4,
         num_kw_block=4,
+        num_au_kw_block=4,
         sok=1,
         eok=1,
         batch_padding_idx=-1,
@@ -112,14 +113,13 @@ class TransformerKWSPhone_noaliloss(nn.Module):
             **au_input_trans_config
         )
         self.au_pos_emb = NM.PositionalEncoding(au_hidden_dim)
-        assert (num_audio_block % 2 == 0)
 
         self.au_transformer_low = nn.ModuleList([
             NM.TransformerLayer(
                 size=au_hidden_dim,
                 self_att=au_self_att(**au_self_att_cofing),
                 feed_forward=NM.FNNBlock(**au_feed_forward_config),
-            ) for _ in range(num_audio_block // 2 - 1)
+            ) for _ in range(num_audio_block - 1)
         ])
 
         self.au_transformer_mid = nn.ModuleList([
@@ -136,7 +136,7 @@ class TransformerKWSPhone_noaliloss(nn.Module):
                 self_att=au_self_att(**au_self_att_cofing),
                 cross_att=au_cross_att(**au_cross_att_config),
                 feed_forward=NM.FNNBlock(**au_feed_forward_config),
-            ) for _ in range(num_audio_block // 2 )
+            ) for _ in range(num_au_kw_block)
         ])
         #self.skip_trans = nn.ModuleList([nn.Linear(au_hidden_dim, au_hidden_dim) for _ in range(num_audio_block // 2 - 1)])
 

8fbb0aa15c7b481a4b134f264049dd2efa3ad0df
WeiYang <qlwy1991@gmail.com>
2025-03-31 16:30:59 +0800

    forget to update __init__.py model file

--- model/__init__.py
+++ model/__init__.py
@@ -10,6 +10,7 @@ from model import TransformerKWSPhone_eval_steps
 from model import TransformerKWSPhone_nocross
 from model import TransformerKWSPhone_nocross_w_ctc
 from model import TransformerKWSPhone_nocross_w_ctc_kw_adapter
+from model import TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear
 
 m_dict = {
     'AEDKWSASR': AEDKWSASR.AEDKWSASR,
@@ -21,5 +22,6 @@ m_dict = {
     'TransformerKWSPhone_eval_steps': TransformerKWSPhone_eval_steps.TransformerKWSPhone_eval_steps,
     'TransformerKWSPhone_nocross': TransformerKWSPhone_nocross.TransformerKWSPhone_nocross,
     'TransformerKWSPhone_nocross_w_ctc': TransformerKWSPhone_nocross_w_ctc.TransformerKWSPhone_nocross_w_ctc,
-    'TransformerKWSPhone_nocross_w_ctc_kw_adapter': TransformerKWSPhone_nocross_w_ctc_kw_adapter.TransformerKWSPhone_nocross_w_ctc_kw_adapter
+    'TransformerKWSPhone_nocross_w_ctc_kw_adapter': TransformerKWSPhone_nocross_w_ctc_kw_adapter.TransformerKWSPhone_nocross_w_ctc_kw_adapter,
+    'TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear': TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear.TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear
 }

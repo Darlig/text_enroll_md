3ac1a26d6dd520a8532df36a8c56240058e3f80a
weiy <qlwy1991@gmail.com>
2025-10-12 13:58:17 +0800

    clean up some code to focus on current work

--- model/__init__.py
+++ model/__init__.py
@@ -1,38 +1,14 @@
 #__all__ = ['DoubleAE']
 # new model append in m_dict
-from model import AEDKWSASR
-from model import AEDKWSASRPhone
-from model import AEDASR
-from model import AEDKWSASRPhoneUnet
-from model import TransformerKWSPhone
-from model import TransformerKWSPhone_noaliloss
-from model import TransformerKWSPhone_eval_steps
-from model import TransformerKWSPhone_nocross
 from model import TransformerKWSPhone_nocross_w_ctc
 from model import TransformerKWSPhone_speech_only
-from model import TransformerKWSPhone_nocross_w_ctc_no_kw
-from model import TransformerKWSPhone_nocross_w_ctc_tmp_debug2_equal_to_no_spec_mask
-from model import TransformerKWSPhone_nocross_w_ctc_tmp_debug1_target_mask
-from model import TransformerKWSPhone_nocross_wo_ctc
 from model import TransformerKWSPhone_nocross_w_ctc_kw_adapter
 from model import TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear
 from model import TransformerKWSPhone_sph_emb_concat_ctc_det
 
 m_dict = {
-    'AEDKWSASR': AEDKWSASR.AEDKWSASR,
-    'AEDKWSASRPhone': AEDKWSASRPhone.AEDKWSASRPhone,
-    'AEDASR': AEDASR.AEDASR,
-    'AEDKWSASRPhoneUnet': AEDKWSASRPhoneUnet.AEDKWSASRPhoneUnet,
-    'TransformerKWSPhone': TransformerKWSPhone.TransformerKWSPhone,
-    'TransformerKWSPhone_noaliloss': TransformerKWSPhone_noaliloss.TransformerKWSPhone_noaliloss,
-    'TransformerKWSPhone_eval_steps': TransformerKWSPhone_eval_steps.TransformerKWSPhone_eval_steps,
-    'TransformerKWSPhone_nocross': TransformerKWSPhone_nocross.TransformerKWSPhone_nocross,
     'TransformerKWSPhone_nocross_w_ctc': TransformerKWSPhone_nocross_w_ctc.TransformerKWSPhone_nocross_w_ctc,
     'TransformerKWSPhone_speech_only': TransformerKWSPhone_speech_only.TransformerKWSPhone_speech_only,
-    'TransformerKWSPhone_nocross_w_ctc_no_kw': TransformerKWSPhone_nocross_w_ctc_no_kw.TransformerKWSPhone_nocross_w_ctc_no_kw,
-    'TransformerKWSPhone_nocross_w_ctc_tmp_debug2_equal_to_no_spec_mask': TransformerKWSPhone_nocross_w_ctc_tmp_debug2_equal_to_no_spec_mask.TransformerKWSPhone_nocross_w_ctc_tmp_debug2_equal_to_no_spec_mask,
-    'TransformerKWSPhone_nocross_w_ctc_tmp_debug1_target_mask': TransformerKWSPhone_nocross_w_ctc_tmp_debug1_target_mask.TransformerKWSPhone_nocross_w_ctc_tmp_debug1_target_mask,
-    'TransformerKWSPhone_nocross_wo_ctc': TransformerKWSPhone_nocross_wo_ctc.TransformerKWSPhone_nocross_wo_ctc,
     'TransformerKWSPhone_nocross_w_ctc_kw_adapter': TransformerKWSPhone_nocross_w_ctc_kw_adapter.TransformerKWSPhone_nocross_w_ctc_kw_adapter,
     'TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear': TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear.TransformerKWSPhone_nocross_w_ctc_kw_adapter_nonlinear,
     'TransformerKWSPhone_sph_emb_concat_ctc_det': TransformerKWSPhone_sph_emb_concat_ctc_det.TransformerKWSPhone_sph_emb_concat_ctc_det
